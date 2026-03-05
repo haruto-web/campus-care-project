c8279439118a38a8f3ada4a076cbadce8ad27fef
venandrewmirasol@gmail.com <venandrewmirasol@gmail.com>
2026-03-05 23:05:14 +0800

    update

--- accounts/otp_utils.py
+++ accounts/otp_utils.py
@@ -1,12 +1,18 @@
 from django.core.mail import send_mail
 from django.conf import settings
+import socket
 
 
 def send_otp_email(email, code):
-    send_mail(
-        subject='Your BrightTrack Verification Code',
-        message=f'Your verification code is: {code}\n\nThis code expires in 10 minutes. Do not share it with anyone.',
-        from_email=settings.DEFAULT_FROM_EMAIL,
-        recipient_list=[email],
-        fail_silently=False,
-    )
+    old_timeout = socket.getdefaulttimeout()
+    socket.setdefaulttimeout(10)
+    try:
+        send_mail(
+            subject='Your BrightTrack Verification Code',
+            message=f'Your verification code is: {code}\n\nThis code expires in 10 minutes. Do not share it with anyone.',
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[email],
+            fail_silently=False,
+        )
+    finally:
+        socket.setdefaulttimeout(old_timeout)
--- campus_care/settings.py
+++ campus_care/settings.py
@@ -179,7 +179,8 @@ LOGOUT_REDIRECT_URL = 'login'
 EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.console.EmailBackend')
 EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
 EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
+EMAIL_USE_SSL = config('EMAIL_USE_SSL', default=False, cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 DEFAULT_FROM_EMAIL = config('EMAIL_HOST_USER', default='noreply@brighttrack.com')
