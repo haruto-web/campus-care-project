ea2cc710328b2a8cb8da904a305dbe9dc50b3729
venandrewmirasol@gmail.com <venandrewmirasol@gmail.com>
2026-03-09 15:01:23 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ python manage.py migrate
 python manage.py migrate sites || true
 python manage.py configure_site || true
 python manage.py create_superuser || true
-python manage.py seed_demo || true
+python manage.py seed_demo || False
